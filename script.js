9d659822eb25f288d7efc0efd7643c213e399614
Şevval Alp <ssevvalalp@gmail.com>
2024-08-20 15:58:06 +0300

    Kodun sadeleştirilmesi

--- script.js
+++ script.js
@@ -6,56 +6,88 @@ const repassword = document.getElementById('repassword');
 
 
 
-function error(input, message){
-    input.className =  'form-control is-invalid'
-    const div = input.nextElementSibling;
-    div.innerText = message;
-    div.className = 'invalid-feedback';
-}
+    function error(input, message){
+        input.className =  'form-control is-invalid'
+        const div = input.nextElementSibling;
+        div.innerText = message;
+        div.className = 'invalid-feedback';
+    }
 
-function success(input){
-    input.className =  'form-control is-valid'
-}
+    function success(input){
+        input.className =  'form-control is-valid'
+    }
 
-const validateEmail = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-  };
+
+    //email regex(regular expression) js
+    function checkEmail (input){
+
+        const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+        if(re.test(input.value)){
+            success(input);
+        }else{
+            error(input, 'hatalı bir email');
+        }
+
+    }
+
+  
+
+
+
+    function checkRequired(inputs){
+    //  console.log(input.value);
+    inputs.forEach(function(input) {    
+    if(input.value === ''){
+        error(input, `${input.id} is required`)
+    }else{
+        success(input);
+    }
+    });
+        
+    }
+
+
 
 //form için submit olayı gerçekleşir
 form.addEventListener('submit', function(event){
 
-    event.preventDefault();
+    event.preventDefault(); //submit olmaması için
     //console.log(username.value);
 
-    if(username.value === ''){
-       error(username , 'username gerekli');
-    }else{
-       success(username);
-    }
+    // if(username.value === ''){
+    //    error(username , 'username gerekli');
+    // }else{
+    //    success(username);
+    // }
 
-    if(email.value === ''){
-        error(email, 'email gerekli');
-    }else if(!validateEmail(email.value)){
-        error(email, 'email formatında giriş yapınız');
-    }
-    else{
-        success(email);
-    }
+    // if(email.value === ''){
+    //     error(email, 'email gerekli');
+    // }else if(!validateEmail(email.value)){
+    //     error(email, 'email formatında giriş yapınız');
+    // }
+    // else{
+    //     success(email);
+    // }
 
-    if(password.value === ''){
-        error(password, 'parola gerekli');
-    }else{
-        success(password);
-    }
+    // if(password.value === ''){
+    //     error(password, 'parola gerekli');
+    // }else{
+    //     success(password);
+    // }
 
-    if(repassword.value === ''){
-        error(repassword, 'parola tekrarı gerekli ');
-    }else{
-        success(repassword);
-    }
+    // if(repassword.value === ''){
+    //     error(repassword, 'parola tekrarı gerekli ');
+    // }else{
+    //     success(repassword);
+    // }
+
+
+    checkRequired([username,email,password,repassword]);
+    // checkRequired(email);
+    // checkRequired(password);
+    // checkRequired(repassword);
+    checkEmail(email);
+
 
 });
